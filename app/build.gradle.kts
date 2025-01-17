b86aecc8c47d37c756b60debe9191040e1ddf9d2
Sahil Gabani <sahilgabani.software@gmail.com>
2025-01-17 16:16:11 +0530

    add jar file option

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,10 @@ android {
 }
 
 dependencies {
+
+
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
